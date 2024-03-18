474cb78942a8def27c7204221d1aadbf1f3daaac
Mitch <mitch@hawkins5.com>
2024-03-18 19:01:21 +1100

    Initial commit

---
+++ src/main/java/io/nology/todolistspringbackend/TodolistSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package io.nology.todolistspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nology/todolistspringbackend/TodolistSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.todolistspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
